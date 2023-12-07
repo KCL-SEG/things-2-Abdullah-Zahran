016fb9ca62b6285bc61a3db3323caa61bb06bf8f
Abdullah Zahran <k22042495@kcl.ac.uk>
2023-12-07 16:58:35 +0000

    Finish exercise

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,19 @@
 """Forms of the project."""
 
 # Create your forms here.
+
+from django import forms
+from .models import Thing
+class ThingForm(forms.ModelForm):
+    class Meta:
+        model = Thing
+        fields = ['name', 'description', 'quantity']
+
+    description = forms.CharField(widget=forms.Textarea)
+    quantity = forms.IntegerField(widget=forms.NumberInput)
+
+    def clean_quantity(self):
+        quantity = self.cleaned_data.get('quantity')
+        if quantity is not None and quantity < 0:
+            raise forms.ValidationError('Quantity must be a non-negative number.')
+        return quantity
